c35da644c02c82507caa31b35f3aff3458a74cfc
Mitul Patel <39785177+bittricky@users.noreply.github.com>
2019-07-09 19:59:26 -0400

    add app layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import AppLayout from './AppLayout';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <AppLayout>
+      
+    </AppLayout>
   );
 }
 
---
+++ src/AppLayout.js
@@ -0,0 +1,5 @@
+import styled from 'style-components';
+
+export default styled.div`
+  padding: 40px;
+`;
