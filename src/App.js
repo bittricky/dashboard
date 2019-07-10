f40aeb08e7316079b301749cdbf373557c290914
Mitul Patel <39785177+bittricky@users.noreply.github.com>
2019-07-09 20:07:08 -0400

    Add appBar and grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+
 import AppLayout from './AppLayout';
+import AppBar from './components/AppBar';
+
 
 function App() {
   return (
     <AppLayout>
-      
+      <AppBar />
     </AppLayout>
   );
 }
---
+++ src/components/AppBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'style-components';
+
+const Bar = styled.div`
+  display: grid;
+  grid-template-columns: 180px auto 100px 100px;
+`;
+
+export default function () {
+  return <Bar>
+    <div>Coin</div>
+    <div>Dashboard</div>
+    </div>Settings</div>
+  </Bar>
+}
