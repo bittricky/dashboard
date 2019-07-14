75c736b771318fbe8109591d62eb5382b01527cc
Mitul Patel <39785177+bittricky@users.noreply.github.com>
2019-07-14 08:37:06 -0400

    add topsection coin grid

--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -2,18 +2,21 @@ import React from 'react';
 import {AppContext} from '../AppProvider';
 import CoinHeaderGrid from './CoinHeaderGrid';
 
-import {SelectableTile} from '../Shared/Tile';
+import {SelectableTile, DisabledTile, DeletableTile} from '../Shared/Tile';
 import CoinImage from '../Shared/CoinImage';
 
-export default function({coinKey}) {
+export default function({coinKey, topSection}) {
 
   return <AppContext.Consumer>
   {({coinList}) => {
     let coin = coinList[coinKey];
-    const TileClass = SelectableTile;
+    let TileClass = SelectableTile;
+    if (topSection) {
+      TileClass = DeletableTile;
+    }
     return <TileClass>
       <CoinImage coin={coin} />
-      <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol} />
+      <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol} topSection={topSection}/>
     </TileClass>
   }}
   </AppContext.Consumer>
